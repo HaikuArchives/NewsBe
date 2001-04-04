258cfe19d45189f14c8dea595398e7dc8bfb8be8
vargol <>
2001-04-04 18:36:30 +0000

    moved to new directory

---
+++ NewsBe/Components/NewsBeMenuBar.cpp
@@ -0,0 +1,104 @@
+// all includes here
+#include "Components/NewsBeMenuBar.h"
+
+//---------------
+// constructor
+//---------------
+
+NewsBeMenuBar::NewsBeMenuBar(BRect frame, const char *name, 
+	uint32 resizingMode, menu_layout layout, bool resizeToFit) :
+	BMenuBar(frame, name, resizingMode, layout, resizeToFit)
+{
+	BMenu *tempMenu;
+	BMenuItem *tempMenuItem;
+	BMessage *pMsg;
+
+	//first menu....
+	tempMenu = new BMenu("News Transfer");
+
+	pMsg = new BMessage(GET_NEWS);
+	tempMenuItem = new BMenuItem("Retrieve News",pMsg,'G'); 	
+	tempMenu->AddItem(tempMenuItem);
+
+	pMsg = new BMessage(SEND_NEWS);
+	tempMenuItem = new BMenuItem("Send News",pMsg,'S'); 	
+	tempMenu->AddItem(tempMenuItem);
+	
+	pMsg = new BMessage(BOTH_NEWS);
+	tempMenuItem = new BMenuItem("Send and Retrieve",pMsg,'B'); 	
+	tempMenu->AddItem(tempMenuItem);	
+
+	AddItem(tempMenu);
+	
+	//second menu....
+	tempMenu = new BMenu("Group Maintanence");
+	
+//	pMsg = new BMessage(EDIT_SERVERS);
+//	tempMenuItem = new BMenuItem("Edit Servers",pMsg,'N'); 	
+//	tempMenu->AddItem(tempMenuItem);	
+	
+	pMsg = new BMessage(EXPIRE_NEWS);
+	tempMenuItem = new BMenuItem("EXPIRE News",pMsg,'E'); 	
+	tempMenu->AddItem(tempMenuItem);
+
+	pMsg = new BMessage(FIX_GROUPS);
+	tempMenuItem = new BMenuItem("Fix Group List",pMsg,'L'); 	
+	tempMenu->AddItem(tempMenuItem);
+	
+	
+	pMsg = new BMessage(TEXT_OPTION_WINDOW);
+	tempMenuItem = new BMenuItem("Text Options",pMsg,'O'); 	
+	tempMenu->AddItem(tempMenuItem);
+
+	pMsg = new BMessage(ARTICLE_OPTION_WINDOW);
+	tempMenuItem = new BMenuItem("Article Options",pMsg,'P'); 	
+	tempMenu->AddItem(tempMenuItem);
+
+	pMsg = new BMessage(USER_OPTION_WINDOW);
+	tempMenuItem = new BMenuItem("User Options",pMsg,'Q'); 	
+	tempMenu->AddItem(tempMenuItem);
+	
+	AddItem(tempMenu);
+	
+	//third menu.....
+	tempMenu = new BMenu("Articles");
+	
+	pMsg = new BMessage(OPEN_ARTWIN);
+	tempMenuItem = new BMenuItem("Article Window",pMsg,'D'); 	
+	tempMenu->AddItem(tempMenuItem);
+
+	
+	pMsg = new BMessage(FORWARD_ARTICLE);
+	tempMenuItem = new BMenuItem("Forward Articles",pMsg,'F'); 	
+	tempMenu->AddItem(tempMenuItem);
+	
+	pMsg = new BMessage(DECODE_ARTICLE);
+	tempMenuItem = new BMenuItem("UUdecode Article",pMsg,'U'); 	
+	tempMenu->AddItem(tempMenuItem);	
+	
+	pMsg = new BMessage(DECODE_MIME);
+	tempMenuItem = new BMenuItem("Decode base64",pMsg,'M'); 	
+	tempMenu->AddItem(tempMenuItem);	
+
+	AddItem(tempMenu);
+
+	//forth menu.....
+//	tempMenu = new BMenu("Scripting");
+//	
+//	pMsg = new BMessage(SCRIPT_SERVER);
+//	tempMenuItem = new BMenuItem("Run Script",pMsg,'R'); 	
+//	tempMenu->AddItem(tempMenuItem);
+//
+//	AddItem(tempMenu);
+
+
+}
+
+//---------------
+// destructor
+//---------------
+
+NewsBeMenuBar::~NewsBeMenuBar()
+{
+// insert your codes here
+}
---
+++ NewsBe/Components/NewsBeMenuBar.h
@@ -0,0 +1,25 @@
+#ifndef NEWSBEMENUBAR_HEADER
+#define NEWSBEMENUBAR_HEADER
+
+// all includes here
+#include <stdio.h>
+#include <Be.h>
+
+#include "constants.h"
+
+// BEGIN (class declaration)
+class NewsBeMenuBar : public BMenuBar
+{
+public:
+// insert public methods and data here
+// constructor
+	NewsBeMenuBar(BRect, const char *, uint32, menu_layout, bool);
+// destructor
+	~NewsBeMenuBar();
+			   
+private:
+// insert private methods and data here
+};
+// END (class declaration)
+
+#endif
