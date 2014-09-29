f3b25c30f1f5d51daa044601b7f12808394db200
Augustin Cavalier <waddlesplash@gmail.com>
2014-09-29 11:08:53 -0400

    Delete unneeded stuff.

--- src/NewsBePPC/Version.h
+++
@@ -1,9 +0,0 @@
-// version header..........
-
-#ifndef NEWSBE_VERSION_H
-#define NEWSBE_VERSION_H
-
-const char sNewsBeVersion[30] = { 'N','e','w','s','B','e',' ','1','.','6','.','4','\0' };							
-const char sProcessor[6] = {'P','P','C','\0'};
-
-#endif
--- src/REBOL scripts/test.r
+++
@@ -1,5 +0,0 @@
-#!/boot/home/config/bin/rebol  
-
-REBOL [Title: "REBOL Comms Demo Script"]
-
-print system/script/args
